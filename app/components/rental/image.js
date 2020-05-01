e4be7205eec35e1ef69a56f53387ae68711a7d1b
richardwhitefoot <mouseholdonline@gmail.com>
2020-05-01 10:44:51 +0100

    feat: interactive components - on click event
    Signed-off-by: richardwhitefoot <mouseholdonline@gmail.com>

---
+++ app/components/rental/image.js
@@ -0,0 +1,12 @@
+// NB use: ember generate component-class rental/image
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class RentalImageComponent extends Component {
+    @tracked isLarge = false;
+
+    @action toggleSize() {
+      this.isLarge = !this.isLarge;
+    }
+}
