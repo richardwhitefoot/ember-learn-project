8a88d38a864c38a7651f3ccf326935252e3c8c7d
richardwhitefoot <mouseholdonline@gmail.com>
2020-05-01 10:15:24 +0100

    fix: Add missing files
    Signed-off-by: richardwhitefoot <mouseholdonline@gmail.com>

---
+++ tests/acceptance/super-rentals-test.js
@@ -0,0 +1,70 @@
+import { module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+// NB use: 
+// ember generate acceptance-test super-rentals
+// ember test --server OR ember t -s
+module('Acceptance | super rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function(assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
+    assert.dom('h2').hasText(`Rich's Ember Learning Project!`);
+
+    assert.dom('.jumbo a.button').hasText('About Us - Not much to know');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
+  });
+
+  test('visiting /about', async function(assert) {
+    await visit('/about');
+
+    assert.equal(currentURL(), '/about');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
+    assert.dom('h2').hasText('About My Test Project');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+  });
+
+  test('visiting /getting-in-touch', async function(assert) {
+    await visit('/getting-in-touch');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('nav').exists();
+    assert.dom('h1').hasText('SuperRentals');
+    assert.dom('h2').hasText('Contact Us - We will reply!');
+
+    assert.dom('a.button').hasText('About');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
+  });
+
+  test('navigating using the nav-bar', async function(assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals')
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
+  });
+});
